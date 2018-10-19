2fe577dcc3be264a381286b986ae2efc1e9c1187
Law Yang <lawtouayang@gmail.com>
2018-10-19 10:51:22 -0500

    react dom router

--- server/server.js
+++ server/server.js
@@ -8,6 +8,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files
 app.use(express.static('build'));
 
+app.get('/api/getList', (req,res) => {
+    // var list = ["item1", "item2", "item3"];
+    res.json('helllo backend');
+    console.log('Sent list of items');
+});
+
+
 // App Set //
 const PORT = process.env.PORT || 3000;
 
