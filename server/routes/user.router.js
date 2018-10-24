cf6e1fbe023e9413310c557033093b9e47ebb68e
Law Yang <lawtouayang@gmail.com>
2018-10-24 00:46:20 -0500

    passport/user strategy initialized

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+const encryptLib = require('../modules/encryption');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const userStrategy = require('../strategies/user.strategy');
+
 
 
 router.get('/', (req, res) => {
@@ -8,16 +12,39 @@ router.get('/', (req, res) => {
     res.sendStatus(200);
 });
 
-router.get('/user', (req, res) => {
-    const queryText = `GET * FROM users`;
-    pool.query(queryText)
+router.get('/', rejectUnauthenticated, (req, res) => {
+    // sends back user object from the database
+    res.send(req.user);
+})
+
+// create new user
+// handles post request with new user data
+// Will encrpyt the password before it gets inserted into the database
+router.post('/register',(req, res, next) => {
+    const username = req.body.username;
+    const password = encryptLib.encriptPassword(req.body.password);
+    const queryText = `INSERT INTO users (username, hash) VALUES $1, $2 RETURNING id`;
+    pool.query(queryText, [username, password])
         .then((result) => {
-            console.log(`back from the database with the users`, result);
-            res.send(result.rows);
-        }).catch((error) => {
-            console.log(`error getting users`, error);
-            res.sendState(500);
+            console.log(`successfully created new user`);
+            res.sendStatus(200);
         })
+        .catch((error) => {
+            console.log('Error creating new user', error);
+            res.sendStatus(500);
+        })
+})
+
+//login
+router.post('/login', userStrategy.authenticate('local'), (req, res) => {
+    res.sendStatus(200);
+});
+
+// logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    console.log(`successfully logged out`);
+    res.sendStatus(200);
 })
 
 module.exports = router;
